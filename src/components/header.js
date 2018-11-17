459f126228be86a8e5f4893043952859159d3422
Ross Cournoyer <rcoupvc@yahoo.com>
2018-11-17 14:42:59 +0800

    Add routing to header, and redux boilerplate

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default () => {
 	return (
 		<div className="header">
 			<ul className="header-ul">
-				<li className="header-li logo">Frendo</li>
-				<li className="header-li">Sign Up</li>
-				<li className="header-li">Sign In</li>
+				<Link className="link" to="/"><li className="header-li logo">Frendo</li></Link>
+				<Link className="link" to="/signup"><li className="header-li">Sign Up</li></Link>
+				<Link className="link" to="/signin"><li className="header-li">Sign In</li></Link>
 			</ul>
 		</div>
-);
+	);
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { render } from 'react-dom'
+// import { createStore, applyMiddleware } from 'redux';
+// import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './components/app';
 import Header from './components/header';
 import style from './scss/main.scss';
 
+// const store = createStore();
+
 render((
-  <Router>
-    <App style={style}>
-      <Header />
-    </App>
-  </Router>
+	// <Provider store={store}>
+		<Router>
+			<App style={style}>
+				<Header />
+			</App>
+		</Router>
+	// </Provider>
 ), document.getElementById('root'));
