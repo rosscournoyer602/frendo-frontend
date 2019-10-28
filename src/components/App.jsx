5f576ad4c9884518cfbc0f8759d6f328eeb0d118
Ross Cournoyer <rcoupvc@yahoo.com>
2019-10-28 22:34:42 +0800

    Websocket connection

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Header from './Header';
 import MobileHeader from './MobileHeader';
 import Main from './Main';
+import WebSocketHOC from './WebSocketHOC';
 
 const App = () => (
-  <>
+  <WebSocketHOC>
     <Header />
     <MobileHeader />
     <Main />
-  </>
+  </WebSocketHOC>
 );
 
 export default App;
---
+++ src/components/WebSocketHOC.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class WebSocketHOC extends Component {
+  componentDidMount() {
+    // eslint-disable-next-line no-undef
+    const ws = new WebSocket('ws://localhost:8080');
+
+    ws.onopen = () => {
+      console.log('connected');
+      ws.send('Hello Server, I am the Client!');
+    };
+
+    ws.onmessage = e => {
+      const message = e.data;
+      console.log(message);
+    };
+
+    ws.onclose = () => {
+      console.log('disconnected');
+    };
+  }
+
+  render() {
+    const { children } = this.props;
+    return <>{children}</>;
+  }
+}
+
+WebSocketHOC.propTypes = {
+  children: PropTypes.node.isRequired
+};
