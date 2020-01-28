c7564436fd875e518dd0db0cffb7e462e797998c
Ross Cournoyer <rcoupvc@yahoo.com>
2020-01-28 12:38:56 +0800

    Finish basic style and alignment of chat

--- src/components/RequireAuth.jsx
+++ src/components/RequireAuth.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { browserHistory } from 'react-router';
 
@@ -25,6 +26,9 @@ export default function(ComposedComponent) {
   function mapStateToProps(state) {
     return { authStatus: state.authStatus };
   }
+  Authentication.propTypes = {
+    authStatus: PropTypes.bool.isRequired
+  };
 
   return connect(mapStateToProps)(Authentication);
 }
